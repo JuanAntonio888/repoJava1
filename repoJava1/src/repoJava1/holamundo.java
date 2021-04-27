af1a847f121d893b1db88956e871fcfc8d8f60b4
JuanAntonio888 <83217528+JuanAntonio888@users.noreply.github.com>
2021-04-27 09:22:09 +0200

    Update holamundo.java

--- repoJava1/src/repoJava1/holamundo.java
+++ repoJava1/src/repoJava1/holamundo.java
@@ -5,7 +5,8 @@ public class holamundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-		System.out.println("adios");
+		//System.out.println("adios");
+		System.out.println("hasta luego");
 	}
 
 }
